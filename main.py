9d3ff7ce5b6f14d14e0845744b8c4919b494526a
Zengyu-Yan <zoe.yan@porsche.digital>
2024-11-07 15:22:55 +0100

    add url

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 # Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000","https://zindex-zyn.vercel.app"],  # Your React app URL
+    allow_origins=["http://localhost:3000","https://zindex-zyn.vercel.app","https://www.z-index.org","https://z-index.org"],  # Your React app URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
